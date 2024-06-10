b0bfe94f38ed35c80ed00be1cf4bb27ff818f04a
dongheon0827 <dongheon827@naver.com>
2024-06-10 10:07:31 +0900

    NaverOAuth, Authorization

--- src/main/kotlin/org/study/todobackend/domain/member/authentication/controller/AuthenticationController.kt
+++ src/main/kotlin/org/study/todobackend/domain/member/authentication/controller/AuthenticationController.kt
@@ -1,4 +1,25 @@
 package org.study.todobackend.domain.member.authentication.controller
 
-class AuthenticationController (){
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.study.todobackend.domain.member.authentication.dto.LoginRequest
+import org.study.todobackend.domain.member.authentication.dto.LoginResponse
+import org.study.todobackend.domain.member.authentication.dto.SignUpRequest
+import org.study.todobackend.domain.member.authentication.service.AuthService
+
+class AuthenticationController(
+    private val authService: AuthService
+) {
+    @PostMapping("/sign-up")
+    fun signUp(@RequestBody signUpRequest: SignUpRequest): ResponseEntity<Unit> {
+        authService.signUp(signUpRequest)
+        return ResponseEntity.status(HttpStatus.CREATED).build()
+    }
+
+    @PostMapping("/login")
+    fun login(@RequestBody loginRequest: LoginRequest): ResponseEntity<LoginResponse> {
+        return ResponseEntity.status(HttpStatus.OK).body(authService.login(loginRequest))
+    }
 }
